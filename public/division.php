592c0c334406b000bef6e72a8b829e52f1f9b66b
itrascastro <i.trascastro@gmail.com>
2013-12-06 21:30:32 +0100

    First commit

---
+++ public/division.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora - Division</title>
+    <meta name="charset" content="utf-8">
+</head>
+<body>
+<?php echo $_POST['op1'] / $_POST['op2']; ?>
+</body>
+</html>
---
+++ public/sub.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora - Sub</title>
+</head>
+<body>
+<?php echo $_POST['op1'] - $_POST['op2']; ?>
+</body>
+</html>
