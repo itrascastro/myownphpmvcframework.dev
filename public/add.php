9e13c0a6c9615d40afd4a077d8682d6a62cdc343
itrascastro <i.trascastro@gmail.com>
2013-12-06 21:30:32 +0100

    A non MVC application

---
+++ public/add.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora - Add</title>
+</head>
+<body>
+<?php echo $_POST['op1'] + $_POST['op2']; ?>
+</body>
+</html>
---
+++ public/multiplication.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora - Multiplication</title>
+</head>
+<body>
+<?php echo $_POST['op1'] * $_POST['op2']; ?>
+</body>
+</html>
